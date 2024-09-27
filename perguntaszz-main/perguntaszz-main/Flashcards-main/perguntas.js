aa39d4f25b6ba203480b89c1ff367076adb9cb84
Pedroodpes <144038965+Pedroodpes@users.noreply.github.com>
2024-09-27 11:00:19 -0300

    Add files via upload

---
+++ perguntaszz-main/perguntaszz-main/Flashcards-main/perguntas.js
@@ -0,0 +1,137 @@
+criaCartao(
+    'futebol',
+    'Quem é o maior artilheiro das seleçoes?',
+    'CRISTIANO RONALDOOO, CR7'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Alemanha?',
+    'Berlin'
+)
+
+criaCartao(
+    'Historia',
+    'Em que ano a segumda guerra mundial acabou?',
+    '1945'
+)
+
+criaCartao(
+    'Inglês',
+    'Como se diz mercado em inglês?',
+    'Market'
+)
+
+criaCartao(
+    'Matemática',
+    'Qual é a raiz quadrada de 484?',
+    '22'
+)
+
+criaCartao(
+    'Química',
+    'O que são protons e eletrons?',
+    'protons são cargas positivas e eletrons são cargas negativas'
+)
+
+criaCartao(
+    'Politica',
+    'Quantos presidentes da constituição Brasileira foram presos no século 21?',
+    'luiz inacio lula da silva e michel temer'
+)
+
+criaCartao(
+    'esportes',
+    'qual o maior clube europeu?',
+    'Real Madrid'
+)
+
+criaCartao(
+    'quimica',
+    'qual a formula da agua?',
+    'H2O'
+)
+
+criaCartao(
+    'matematica',
+    'qual a soma de 77+33',
+    '110'
+)
+
+criaCartao(
+    'geografia',
+    'qual o pais com mais habitantes',
+    'india'
+)
+
+criaCartao(
+    'biologia',
+    'quem criou a teoria da evoluçã?',
+    'Charles Darwin'
+)
+
+criaCartao(
+    'historia',
+    'Quem matou Adolf Hitler?',
+    'hitler'
+)
+
+criaCartao(
+    'esportes',
+    'qual foi o atleta mais dominante da historia?',
+    'Michael Phelps'
+)
+
+criaCartao(
+    'olimpiadas',
+    'Quando foi criada as olimpiadas?',
+    '1896'
+)
+
+criaCartao(
+    'geografia',
+    'qual o maior continente do mundo?',
+    'Asia'
+)
+
+criaCartao(
+    'artes',
+    'quem fez a monalisa?',
+    'Leonardo da Vinci'
+)
+
+criaCartao(
+    'Fisica',
+    'qual é a definição de gravidade?',
+    'é o fenômeno de atração que comanda a movimentação dos objetos.'
+)
+
+criaCartao(
+    'historia',
+    'quem foi o primeiro presitente do Brasil?',
+    'Deodoro da Fonseca'
+)
+
+criaCartao(
+    'geral ',
+    'quem criou o chat gpt?',
+    'Open Ia'
+)
+
+criaCartao(
+    'artes',
+    'quem fez a noite estrelada?',
+    'van gogh'
+)
+
+criaCartao(
+    'biologia',
+    'o qu é uma célula?',
+    'Uma unidade basica da vida'
+)
+
+criaCartao(
+    'artes',
+    'quem fez a monalisa?',
+    'Leonardo da Vinci'
+)
